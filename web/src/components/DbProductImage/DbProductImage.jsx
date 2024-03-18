2184d3a8bc30d1813616c4a624b99e0fe8dd0385
Joe Thompson <jthompson7@blueorigin.com>
2024-03-18 12:26:36 -0700

    Add image loading spinner.

--- web/src/components/DbProductImage/DbProductImage.jsx
+++ web/src/components/DbProductImage/DbProductImage.jsx
@@ -5,17 +5,51 @@ import { ref, getDownloadURL } from 'firebase/storage'
 import { storage } from 'src/lib/firebase'
 
 const DbProductImage = ({ product }) => {
-  const [url, setUrl] = useState('https://placehold.co/200x200')
+  const [url, setUrl] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     if (product.images.length > 0) {
-      getDownloadURL(ref(storage, product.images[0].url)).then((url) =>
+      getDownloadURL(ref(storage, product.images[0].url)).then((url) => {
         setUrl(url)
-      )
+        setLoading(false)
+      })
+    } else {
+      setLoading(false)
     }
   }, [product.images])
 
-  return <img src={url} alt={product.name} className="image" />
+  if (url === null && !loading) {
+    return (
+      <div className="loader__container">
+        <svg
+          className="placeholder__image"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#000000"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path d="M7.828 5l-1-1H22v15.172l-1-1v-.69l-3.116-3.117-.395.296-.714-.714.854-.64a.503.503 0 0 1 .657.046L21 16.067V5zM3 20v-.519l2.947-2.947a1.506 1.506 0 0 0 .677.163 1.403 1.403 0 0 0 .997-.415l2.916-2.916-.706-.707-2.916 2.916a.474.474 0 0 1-.678-.048.503.503 0 0 0-.704.007L3 18.067V5.828l-1-1V21h16.172l-1-1zM17 8.5A1.5 1.5 0 1 1 15.5 7 1.5 1.5 0 0 1 17 8.5zm-1 0a.5.5 0 1 0-.5.5.5.5 0 0 0 .5-.5zm5.646 13.854l.707-.707-20-20-.707.707z"></path>
+            <path fill="none" d="M0 0h24v24H0z"></path>
+          </g>
+        </svg>
+      </div>
+    )
+  } else if (loading) {
+    return (
+      <div className="loader__container">
+        <div className="loader"></div>
+      </div>
+    )
+  } else {
+    return <img src={url} alt={product.name} className="image" />
+  }
 }
 
 export default DbProductImage
