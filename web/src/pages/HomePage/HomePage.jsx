c46d9404cd1edb55e976ed2c5b1192fe36f66569
Joe Thompson <jthompson7@blueorigin.com>
2024-04-18 16:29:34 -0700

    Add order confirmation listener.

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
+
 import { Metadata } from '@redwoodjs/web'
 
 import ProductCardsCell from 'src/components/ProductCardsCell'
 
 const HomePage = () => {
+  useEffect(() => {
+    document.addEventListener('snipcart.ready', () => {
+      // You can safely use window.Snipcart here
+      window.Snipcart.events.on('cart.confirmed', (cartConfirmResponse) => {
+        console.log(cartConfirmResponse)
+      })
+
+      window.Snipcart.events.on('cart.confirm.error', (confirmError) => {
+        console.log(confirmError)
+      })
+    })
+  }, [])
+
   return (
     <>
       <Metadata title="Home" description="Home page" />
